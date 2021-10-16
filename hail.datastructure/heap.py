b7f8a257a2eeb66b917d0885fb217ee7997683a1
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-10-17 00:50:33 +0900

    heapify 추가

--- hail.datastructure/heap.py
+++ hail.datastructure/heap.py
@@ -4,6 +4,26 @@ class Heap:
     self.reverse = reverse
     self.length = len(elements)
 
+    self.__heapify()
+
+  def __heapify(self):
+    for i in reversed(range(self.length // 2)):
+      while i < self.length:
+        i_left = 2*i + 1
+        i_right = i_left + 1
+
+        if i_left > self.length - 1:
+          break
+        elif i_right > self.length - 1:
+          i_cmp = i_left
+        else:
+          i_cmp = i_left if (self.elements[i_left] < self.elements[i_right]) ^ self.reverse else i_right
+
+        if (self.elements[i] > self.elements[i_cmp]) ^ self.reverse:
+          self.elements[i], self.elements[i_cmp] = self.elements[i_cmp], self.elements[i]
+
+        i = i_cmp
+
   def add(self, value):
     self.elements.append(value)
     
