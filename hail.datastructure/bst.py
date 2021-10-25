08956cf761f485d348a0492fd3d9831d07fd445b
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-10-25 20:54:07 +0900

    이진 탐색 트리 인터페이스만 생성

---
+++ hail.datastructure/bst.py
@@ -0,0 +1,16 @@
+from .node import Node
+
+class BST:
+
+  def __init__(self, reverse=False):
+    self.root = None
+    self.reverse = reverse
+
+  def insert(self, value):
+    pass
+
+  def remove(self, value):
+    pass
+
+  def search(self, value):
+    pass
