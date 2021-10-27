022ca0eac2d47129d205bda32ec2b66d3ff110cf
hail-kang <hail1225@naver.com>
2021-10-27 14:44:53 +0900

    search, delete구현

--- datastructure/bst.py
+++ datastructure/bst.py
@@ -22,10 +22,33 @@ class BST:
         node.left = Node(value)
       else:
         node.right = Node(value)
-      
 
-  def remove(self, value):
-    pass
+  def delete(self, value):
+    node = self.search(value)
+    if node == None:
+      raise Exception("Can't find value")
+
+    while node != None:
+      if node.right == None:
+        if node.left == None:
+          break
+        else:
+          node.value = node.left.value
+          node = node.left
+      else:
+        node.value = node.right.value
+        node = node.right
 
   def search(self, value):
-    pass
+      next_node = self.root
+      while next_node != None:
+        node = next_node
+        if value == node.value:
+          return node
+        elif value < node.value:
+          next_node = node.left
+        else:
+          next_node = node.right
+      return None
+
+
