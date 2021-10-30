4ba525f67e20497e8c8b6914aac197feb9826d79
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-10-30 19:55:42 +0900

    AVL 트리 클래스 추가

---
+++ datastructure/avltree.py
@@ -0,0 +1,17 @@
+from .node import Node
+
+class AVL:
+
+  def __init__(self, reverse=False):
+    self.root = None
+    self.reverse = reverse
+
+  def insert(self, value):
+    if self.root == None:
+      self.root = Node(value)
+
+  def delete(self, value):
+    pass
+
+  def search(self, value):
+    pass
