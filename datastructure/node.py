a1de51b4cf0c98210b6c2792c7d2511ce1e4aaaf
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-11-02 23:39:49 +0900

    노드에 parent속성 생성, depth와 balanceFactor메소드 생성

--- datastructure/node.py
+++ datastructure/node.py
@@ -1,7 +1,28 @@
 class Node:
 
-  def __init__(self, value, left=None, right=None):
+  def __init__(self, value, left=None, right=None, parent=None):
     self.value = value
     self.left = left
     self.right = right
+    self.parent = parent
+  
+  def getDepth(self):
+    if self.left == None and self.right == None:
+      return 0
+    elif self.left != None and self.right == None:
+      return self.left.depth()
+    elif self.left == None and self.right != None:
+      return self.right.depth()
+    else:
+      return max(self.left.depth(), self.right.depth())
+    
+  def getBalanceFactor(self):
+    if self.left == None and self.right == None:
+      return 0
+    elif self.left != None and self.right == None:
+      return self.left.depth()
+    elif self.left == None and self.right != None:
+      return -self.right.depth()
+    else:
+      return self.left.depth() - self.right.depth()
       
