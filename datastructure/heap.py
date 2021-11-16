530b0ecec55505dab6a44c0f74d7a8206dcaa937
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-11-16 23:04:45 +0900

    heapify에 down_heap 적용

--- datastructure/heap.py
+++ datastructure/heap.py
@@ -27,21 +27,7 @@ class Heap:
 
   def __heapify(self):
     for i in reversed(range(self.length // 2)):
-      while i < self.length:
-        i_left = 2*i + 1
-        i_right = i_left + 1
-
-        if i_left > self.length - 1:
-          break
-        elif i_right > self.length - 1:
-          i_cmp = i_left
-        else:
-          i_cmp = i_left if (self.elements[i_left] < self.elements[i_right]) ^ self.reverse else i_right
-
-        if (self.elements[i] > self.elements[i_cmp]) ^ self.reverse:
-          self.elements[i], self.elements[i_cmp] = self.elements[i_cmp], self.elements[i]
-
-        i = i_cmp
+      self.__down_heap(i)
 
   def push(self, value):
     self.elements.append(value)
